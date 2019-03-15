a548c5e62726d40a4712ed9469ed4cac480eb9cd
Ted Pattison <ted@tedpattison.net>
2019-03-15 10:32:10 -0400

    Updates

--- Scripts/Fodder/Step1-FindServicePrincipalId.ps1
+++
@@ -1,8 +0,0 @@
-﻿# connect to Azure AD
-$authResponse = Connect-AzureAD
-
-$adApplicationName = "App-only Demo App"
-
-$servicePrincipal = Get-AzureADServicePrincipal -Filter "DisplayName eq '$adApplicationName'"
-
-$servicePrincipal | Format-List DisplayName, AppId, ObjectId
